941e23954c11a9445b339b2d284dca3337acdd0a
Leonid Bushuev <leonidos@gmail.com>
2026-07-01 23:42:25 +0200

    Microsoft: create user Tourist with default schema Zoo

--- DB/Microsoft/Zoo-25/Menagerie/create-Database.sql
+++ DB/Microsoft/Zoo-25/Menagerie/create-Database.sql
@@ -2,6 +2,7 @@
 use master
 go
 
+-- DATABASE
 create database Menagerie
     on primary (name = 'Menagerie',
                 filename = '/var/opt/mssql/data/Menagerie.mdf',
@@ -17,23 +18,38 @@ alter database Menagerie set
     recovery simple
 go
 
-create login Curator with password = 'chap', check_policy = OFF
+-- LOGINS
+create login Curator
+    with password = 'chap',
+         check_policy = OFF,
+         default_database = Menagerie
 go
 
+create login Tourist
+    with password = 'tour',
+         check_policy = OFF,
+         check_expiration = OFF,
+         default_database = Menagerie
+go
+
+
+-- ENTER into the database MENAGERIE
 use Menagerie
 go
 
+exec sp_addextendedproperty @name='Caption', @value=N'Zoopark'
+exec sp_addextendedproperty @name='Note', @value=N'A lot of different objects'
+exec sp_addextendedproperty @name=N'MS_Description', @value=N'DataGrip Static Tests Database'
+go
+
+
+-- User Curator
 create user Curator from login Curator
 go
 
 exec sp_addrolemember 'db_ddladmin', 'Curator'
 exec sp_addrolemember 'db_datareader', 'Curator'
 exec sp_addrolemember 'db_datawriter', 'Curator'
-go
-
-exec sp_addextendedproperty @name='Caption', @value=N'Zoopark'
-exec sp_addextendedproperty @name='Note', @value=N'A lot of different objects'
-exec sp_addextendedproperty @name=N'MS_Description', @value=N'DataGrip Static Tests Database'
 go
 
 create schema Zoo authorization Curator
@@ -44,3 +60,13 @@ go
 
 grant showplan to Curator
 go
+
+-- User Tourist
+create user Tourist
+    from login Tourist
+    with default_schema = Zoo
+go
+
+exec sp_addrolemember 'db_datareader', 'Tourist'
+go
+
